8808d5938db741f178f06359251ff74602840730
Emllyzzhg <emily.k.zhang@student.uts.edu.au>
2026-08-31 21:17:44 +1000

    Initial Commit

---
+++ BudgetBite/BudgetBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBiteApp.swift
+//  BudgetBite
+//
+//  Created by emily zhang on 31/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
